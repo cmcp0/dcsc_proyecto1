997c4cb91d1fd0b10d5787cdeb60b8cd62a86079
cmcp0 <cmcacerespalacio@gmail.com>
2017-08-26 17:57:38 -0500

    cambios a los modelos, se agrega CRUD(parcial) para usuarios

--- cloudprojects/project1/urls.py
+++ cloudprojects/project1/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^new$', views.administrador_new, name='administrador_new'),
     url(r'^create$', views.administrador_create, name='administrador_create'),
 
-    
-
-
+    url(r'^usuario/(?P<id>[0-9]+)$', views.usuarios),
+    url(r'^usuario/$', views.usuarios),
+    # url(r'^usuario/(?P<key>\w{1,50})$', views.usuarios),
 
 ]
