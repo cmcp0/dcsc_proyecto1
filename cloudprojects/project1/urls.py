f2dc720280d4168248f13be35c65d0c908ed4f04
majimauni <ma.jimenezm@uniandes.edu.co>
2017-08-30 23:34:57 -0500

    modificado: modelo, urls.py, archivo de migraciones y views.py--CONTIENE LOS CRUD DE LOS TRES MODELOS

--- cloudprojects/project1/urls.py
+++ cloudprojects/project1/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     url(r'^create$', views.administrador_create, name='administrador_create'),
 
     url(r'^usuario/(?P<id>[0-9]+)$', views.usuarios),
-    url(r'^usuario/$', views.usuarios),
+    url(r'^usuario$', views.usuarios),
     # url(r'^usuario/(?P<key>\w{1,50})$', views.usuarios),
+    
+    url(r'^concurso/(?P<id>[0-9]+)$', views.concursos),
+    url(r'^concurso$', views.concursos),
 
+    url(r'^video/(?P<id>[0-9]+)$', views.videos),
+    url(r'^video$', views.videos),
 ]
--- cloudprojects/project1/views.py
+++ cloudprojects/project1/views.py
@@ -18,7 +18,8 @@ def index(request):
 
 def administrador_new(request):
 	context={}
-	return render(request, 'administrador_new.html', context)
+	#return render(request, 'administrador_new.html', context)
+	return HttpResponse("PRUEBA")
 
 @csrf_exempt
 def administrador_create(request):
@@ -33,6 +34,7 @@ def usuarios(request, id=-1):
 		# print(request.scheme)
 		# print(request.GET.__getitem__('key'))
 		# print(id)
+		# 
 		key = request.GET.__getitem__('key')
 		try:
 			user = Usuario.objects.get(_token=key)
@@ -85,3 +87,201 @@ def usuarios(request, id=-1):
 			else:
 				userToDel.delete()
 				return JsonResponse({'mensaje':'Usuario Borrado'})
+
+
+	if metodo == 'PUT':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		user_id= body['id']
+
+		try:
+			user = Usuario.objects.get(id=user_id)
+			
+		except (KeyError, Usuario.DoesNotExist):
+			# save user
+			
+			return JsonResponse({'mensaje': 'Usuario no encontrado'})
+		else:
+			# return error
+			user = Usuario(**body)
+			user.save()
+			return JsonResponse({'correcto': ' Usuario modificado'})			
+
+
+def concursos(request, id=-1):
+	metodo = request.method
+
+	if metodo == 'GET':
+		# print(request.scheme)
+		# print(request.GET.__getitem__('key'))
+		# print(id)
+		key = request.GET.__getitem__('key')
+		try:
+			#válida si el usuario tiene acceso a data
+			usuario = Usuario.objects.get(_token=key)
+		
+		except (KeyError, Usuario.DoesNotExist):
+			 
+			return JsonResponse({'error1': 'Token invalido'})
+		else:
+			# print('val')
+
+			try:
+				if id > -1:
+					concursoToGet = [Concurso.objects.get(id=id)]
+				else:
+					
+					concursoToGet = Concurso.objects.all()
+
+			except (KeyError, Concurso.DoesNotExist):
+				return JsonResponse({'error2': 'Concurso no existe'})
+			else:
+				data = serializers.serialize('json', concursoToGet, fields=('nombreconcu','imagenconcu','urlconcu', 'feini','fefin','premio','administraconcu' ))
+				return JsonResponse(data, safe=False)
+
+
+	if metodo == 'POST':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		concurso_url = body['urlconcu']
+
+		try:
+			concuso_ = Concurso.objects.get(urlconcu=concurso_url)
+		except (KeyError, Concurso.DoesNotExist):
+			# save user
+			concurso_ = Concurso(**body)
+			concurso_.save()
+			return JsonResponse({'mensaje': 'Concurso guardado'})
+		else:
+			# return error
+			return JsonResponse({'error': 'Ya existe el Concurso'})				
+
+
+	if metodo == 'DELETE':
+		# print(QueryDict(request.get_full_path().split("?")[1]).get('key'))
+		print(id)
+		_sessionToken = QueryDict(request.get_full_path().split("?")[1]).get('key')
+
+		try:
+			usuario = Usuario.objects.get(_token=_sessionToken)
+		except (KeyError, Usuario.DoesNotExist):
+			return JsonResponse({'error': 'Usuario invalido'})
+		else:
+			try:
+				if id > -1:
+					concursoToDel = Concurso.objects.get(id=id)
+				else:
+					return JsonResponse({'error': 'Concurso no encontrado'})	
+
+			except (KeyError, Concurso.DoesNotExist):
+				return JsonResponse({'error': 'Concurso no existe'})
+			else:
+				concursoToDel.delete()
+				return JsonResponse({'mensaje':'Concurso Borrado'})
+
+
+	if metodo == 'PUT':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		concurso_id= body['id']
+
+		try:
+			concurso_ = Concurso.objects.get(id=concurso_id)
+			
+		except (KeyError, Concurso.DoesNotExist):
+			# save user
+			
+			return JsonResponse({'mensaje': 'Concurso no encontrado'})
+		else:
+			# return error
+			concurso_ = Concurso(**body)
+			concurso_.save()
+			return JsonResponse({'correcto': ' Concurso modificado'})			
+
+def videos(request, id=-1):
+	metodo = request.method
+
+	if metodo == 'GET':
+		# print(request.scheme)
+		# print(request.GET.__getitem__('key'))
+		# print(id)
+		key = request.GET.__getitem__('key')
+		try:
+			#válida si el usuario tiene acceso a data
+			usuario = Usuario.objects.get(_token=key)
+		
+		except (KeyError, Usuario.DoesNotExist):
+			 
+			return JsonResponse({'error1': 'Token invalido'})
+		else:
+			# print('val')
+
+			try:
+				if id > -1:
+					videoToGet = [Video.objects.get(id=id)]
+				else:
+					
+					videoToGet = Video.objects.all()
+
+			except (KeyError, Video.DoesNotExist):
+				return JsonResponse({'error2': 'Video no existe'})
+			else:
+				data = serializers.serialize('json', videoToGet, fields=('fechasub','estado','video', 'descrip','particoncu','nombreconcursante','apellidoconcursante','emailconcursante' ))
+				return JsonResponse(data, safe=False)
+
+	if metodo == 'POST':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		concurso_id = body['particoncu_id']
+
+		try:
+			concurso_ = Concurso.objects.get(id=concurso_id)
+			video_ = Video(**body)
+			video_.save()
+			return JsonResponse({'mensaje': 'Video guardado'})
+		except (KeyError, Concurso.DoesNotExist):
+			# save user
+			
+			return JsonResponse({'mensaje': 'Video no guardado'})
+		
+			
+	if metodo == 'PUT':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		video_id= body['id']
+
+		try:
+			video_ = Video.objects.get(id=video_id)
+			
+		except (KeyError, Concurso.DoesNotExist):
+						
+			return JsonResponse({'mensaje': 'Video no encontrado'})
+		
+		else: 
+			# return error
+			video_ = Video(**body)
+			video_.save()
+			return JsonResponse({'correcto': ' Video modificado'})
+						
+
+	if metodo == 'DELETE':
+		# print(QueryDict(request.get_full_path().split("?")[1]).get('key'))
+		print(id)
+		_sessionToken = QueryDict(request.get_full_path().split("?")[1]).get('key')
+
+		try:
+			usuario = Usuario.objects.get(_token=_sessionToken)
+		except (KeyError, Usuario.DoesNotExist):
+			return JsonResponse({'error': 'Usuario invalido'})
+		else:
+			try:
+				if id > -1:
+					videoToDel = Video.objects.get(id=id)
+				else:
+					return JsonResponse({'error': 'Concurso no encontrado'})	
+
+			except (KeyError, Video.DoesNotExist):
+				return JsonResponse({'error': 'Video no existe'})
+			else:
+				videoToDel.delete()
+				return JsonResponse({'mensaje':'Video Borrado'})					
